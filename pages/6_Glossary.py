dbf20141c3e514f53267d1b43c27419a60791b9f
Vu Ngoc Huong Tran <92384719+huongtran182@users.noreply.github.com>
2026-07-17 00:30:50 -0500

    Update 6_Glossary.py

--- pages/6_Glossary.py
+++ pages/6_Glossary.py
@@ -139,6 +139,7 @@ species = [
     ("Cpro", "<i>Cancer productus</i> – Red rock crab"),
     ("Csap", "<i>Callinectes sapidus</i> – Blue crab"),
     ("Dpu", "<i>Daphnia pulex</i> – Common water flea"),
+    ("Eis", "<i>Erimacrus isenbeckii</i> – horsehair crab"),
     ("Eur", "<i>Eurydice</i> – Sand burrowing isopods"),
     ("HoA", "<i>Homarus americanus</i> – American lobster"),
     ("Lma", "<i>Lithodes maja</i> – Norway king crab"),
